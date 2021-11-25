206872aea04880fb894c03911e27fc9e8443f419
vickyalice <56119317+vickyalice@users.noreply.github.com>
2021-11-25 20:12:31 +0800

    增加驗證登入資料庫作法

---
+++ rice/checkLogin.php
@@ -0,0 +1,19 @@
+<?php
+    require_once "connectDB.php";
+
+    $account = $_POST['account'];
+    $password = $_POST['password'];
+    
+    $selSql = "SELECT `account`,`password` FROM `user`";
+    $selResult = mysqli_query($link,$selSql);
+    $fieldInfo = mysqli_fetch_field($selResult);
+    while($row = mysqli_fetch_array($selResult,MYSQLI_ASSOC)){
+        if($row['account'] == $account & $row['password']==$password){
+            echo "<script>alert('登入成功');location.href='riceType.html';</script>";
+        }else{
+            continue;
+        }
+    }
+    echo "<script>alert('帳號或密碼輸入錯誤，登入失敗');location.href='login.html';</script>";
+
+?>
